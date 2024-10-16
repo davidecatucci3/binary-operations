992864711c07f6f413093c2fae4a20725ba4de9a
Your Name <you@example.com>
2024-10-16 16:37:57 +0200

    fix mistakes and implement dec to bin with bot un

--- README.md
+++ README.md
@@ -4,20 +4,23 @@ This program performs various operations with binary numbers, including conversi
 ## List of operations
 
 ### 1. Conversions
-   The conversion works as well for signed, unsigned and IEEE 754
+   The conversion works as well for signed, unsigned (two's complement) and IEEE 754 (floating-point)
+
    - **to Binary**: Convert each base you want to binary
    - **to Hexadecimal**: Convert each base you want to hexadecimal
    - **to Decimal**: Convert each base you want to decimal
    - **to Custom base**: convert each base you want to a custom base
 
 ### 2. Shifting Operations
-   The conversion works as well for signed, unsigned and IEEE 754
+   The conversion works as well for signed, unsigned (two's complement) and IEEE 754 (floating-point)
+
    - **Logical Shifts**: Shift bits to the left or right by a specified number of positions and fill the rest with 0's
    - **Arithmetic Shifts**: Shift bits to the left or right by a specified number of positions and fill the rest with the signed of the number
    - **Circular Shifts**: Rotate bits to the left or right circularly
 
 ### 3. Arithmetic Operations
-   The conversion works as well for signed, unsigned and IEEE 754
+   The conversion works as well for signed, unsigned (two's complement) and IEEE 754 (floating-point)
+   
    - **Addition / Subtraction**: Perform addition and subtraction of binary numbers 
    - **Multiplication / Division**: Perform multiplication and division of binary numbers
 
--- conversions/to_binary.py
+++ conversions/to_binary.py
@@ -1,13 +1,64 @@
 class to_bin:
     def __init__(self, from_base):
+        '''
+        from_base: number to convert
+        type: unsigned (u), signed (s) or fraction (f)
+        '''
+
         self.from_base = from_base
+
+    @staticmethod
+    def add_plus_one(a, b):
+        res = ''
+
+        b =  b.zfill(len(a))
+
+        carry = '0'
+
+        for i, j in zip(a[::-1], b[::-1]):
+            if i == '1' and j == '1' and carry == '0':
+                res += '0'
+
+                carry = '1'
+            elif i == '1' and j == '1' and carry == '1':
+                res += '1'
+
+                carry = '1'
+            elif ((i == '1' and j == '0') or (i == '0' and j == '1')) and carry == '0':
+                res += '1'
+            elif ((i == '1' and j == '0') or (i == '0' and j == '1')) and carry == '1':
+                res += '0'
+
+                carry = '1'
+            elif i == '0' and j == '0':
+                res += carry
+
+                carry = '0'
+  
+        res = res[::-1]
+
+        return res
+
+    def check_type(self):
+        '''
+        check type: unsigned (u), signed (s) or fraction (f)
+        '''
+
+        type = ''
+
+        if self.from_base < 0:
+            type = 's'
+        else:
+            type = 'u'
+
+        return type
 
     def is_dec(self):
         '''
         check if a number is decimal
         '''
         
-        if str(self.from_base).isdigit():
+        if str(abs(self.from_base)).isdigit():
             return True
             
         return False
@@ -36,6 +87,8 @@ class to_bin:
         elif isinstance(self.from_base, int):
             # check if is not boolean, because True is considered also as int (1)
             if self.is_dec():
+                self.type = self.check_type()
+
                 res = self.dec_to_bin()
               
                 return res
@@ -76,28 +129,64 @@ class to_bin:
                 res += self.dec_to_bin(dic[i])
    
         return res
+    
+    @staticmethod
+    def dec_to_bin_2(x):
+        if x == 0:
+            return '0'
+        
+        res = ''
+
+        while x > 0:            
+            if x % 2 == 0:
+                res += '0'
+            else:
+                res += '1'
+
+            x //= 2
+            
+        res = res[::-1]
+
+        return res
 
     def dec_to_bin(self, from_base=None):
         '''
         convert dec to bin
         '''
 
+        res = ''
+
         # use this method because i need to re-use dec_to_bin() in hex_to_bin()
         if from_base == None:
             from_base = self.from_base
-
-        res = ''
-
-        while from_base > 0:            
-            if from_base % 2 == 0:
-                res += '0'
-            else:
-                res += '1'
-
-            from_base //= 2
         
-        res = res[::-1]
- 
+        if from_base == 0:
+            return '0'
+
+        if self.type == 'u':
+            while from_base > 0:            
+                if from_base % 2 == 0:
+                    res += '0'
+                else:
+                    res += '1'
+
+                from_base //= 2
+            
+            res = res[::-1]
+        elif self.type == 's':
+            abs_val = abs(self.from_base)
+     
+            res = self.dec_to_bin_2(abs_val)
+        
+            l_res = list(res)
+
+            res = ''.join(['0' if i == '1' else '1' for i in l_res])
+
+            res = self.add_plus_one(res, '1')
+
+            if res[0] == '0':
+                res = '1' + res
+         
         return res
 
     def custom_base_to_bin(self):
@@ -105,19 +194,3 @@ class to_bin:
         convert custom base to bin
         '''
 
-# hex to bin
-x5 = hex(12)
-x6 = '0x234D'
-x7 = '3FSF'
-
-print(to_bin(x5)())
-
-# dec to bin
-x1 = 12
-x2 = 1
-x3 = 0
-x4 = 45
-
-print(to_bin(x1)())
-
-# custom base to  bin
